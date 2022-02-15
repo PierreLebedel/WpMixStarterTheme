af650c05041eefed3a24202c12ee784907f339c1
Pierre Lebedel <pierrelebede@gmail.com>
2022-02-15 13:24:29 +0100

    acf syncronize folder

--- functions/assets.php
+++ functions/assets.php
@@ -5,11 +5,13 @@ function wpmixstartertheme_enqueue_script(){
 
     // registers scripts and stylesheets
 	wp_register_style('app', get_stylesheet_directory_uri().'/dist/app.css', [], '1.0' );
+    wp_register_style('app-hotfix', get_stylesheet_uri(), [], '1.0' );
 	wp_register_script('app', get_stylesheet_directory_uri().'/dist/app.js?v=20220131', [], '1.0', true );
 
 	// enqueue global assets
-	wp_enqueue_script('jquery');
 	wp_enqueue_style('app');
+    wp_enqueue_style('app-hotfix');
+    wp_enqueue_script('jquery');
 	wp_enqueue_script('app');
 
 	wp_localize_script('app', 'themeajax', array( 
