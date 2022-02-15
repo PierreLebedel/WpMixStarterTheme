f641e7b74b218c7e024bd07e785f0a0446c8b9b5
Pierre Lebedel <pierrelebede@gmail.com>
2022-02-15 11:22:59 +0100

    npm minimal dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,28 @@
 
 const mix = require('laravel-mix');
 
-mix.sass('src/sass/app.scss', 'dist/')
-    .js('src/js/app.js', 'dist')
-    .sourceMaps();
+const local = require('./mix-local');
+
+mix.setPublicPath('dist');
+
+mix.autoload({
+    jquery: ['$', 'window.jQuery']
+});
+
+if (local.proxy) {
+    mix.browserSync({
+        proxy: local.proxy,
+        injectChanges: true,
+        open: false,
+        files: [
+            'dist/**/*.{css,js}'
+        ]
+    });
+}
+
+mix.js('src/js/app.js', 'dist/')
+    .sass('src/sass/app.scss', 'dist/');
+
+if (mix.inProduction()) {
+    mix.sourceMaps();
+}
